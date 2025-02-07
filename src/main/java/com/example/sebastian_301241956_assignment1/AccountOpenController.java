de76753d8dcf8ad3121a57350b218c71d9b133cd
Sebastian <sebastian_bdavid@hotmail.com>
2025-02-07 16:10:59 -0500

    feat: setup proper mapping methods - renamed object name in index.html - setup method to handle get requests and to return index html - created postmap method to test out form data

--- src/main/java/com/example/sebastian_301241956_assignment1/AccountOpenController.java
+++ src/main/java/com/example/sebastian_301241956_assignment1/AccountOpenController.java
@@ -1,12 +1,27 @@
 package com.example.sebastian_301241956_assignment1;
 
+import org.springframework.ui.Model;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
 public class AccountOpenController {
-    @GetMapping("/")
-    public String home() {
+
+    @GetMapping("/accountOpen")
+    public String showAccountOpen(Model model) {
+        model.addAttribute("account", new AccountOpen());
         return "index";
     }
+
+    @PostMapping("/accountOpen")
+    public String processAccountOpen(@ModelAttribute AccountOpen account, Model model) {
+        model.addAttribute("account", account);
+        System.out.println("Received account opening request: " + account);
+        return "";
+    }
+
 
 }
