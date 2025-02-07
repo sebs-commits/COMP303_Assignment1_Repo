b7b808f97459e96674c5a40994417fbbbb00b6b9
Sebastian <sebastian_bdavid@hotmail.com>
2025-02-07 12:32:18 -0500

    init

---
+++ src/test/java/com/example/sebastian_301241956_assignment1/Sebastian301241956Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sebastian_301241956_assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sebastian301241956Assignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
