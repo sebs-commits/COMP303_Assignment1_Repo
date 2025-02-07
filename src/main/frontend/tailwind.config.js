bbbb95876430d3e7613f1d4d0a539bbf15707d56
Sebastian <sebastian_bdavid@hotmail.com>
2025-02-07 17:27:40 -0500

    style: created selection of colours

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
     content: ["../resources/templates/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'sd-background': '#004643',  // Dark teal
+                'sd-headline': '#fffffe',    // White for
+                'sd-paragraph': '#abd1c6',   // Light teal
+                'sd-button': '#f9bc60',      // Gold/yellow
+                'sd-button-text': '#001e1d', // Dark color
+                'sd-stroke': '#001e1d',      // Dark color
+                'sd-main': '#e8e4e6',        // Light gray
+                'sd-highlight': '#f9bc60',   // Gold/yellow
+                'sd-secondary': '#abd1c6',   // Light teal
+                'sd-tertiary': '#e16162'     // Red/coral
+            }
+        },
     },
     plugins: [],
 }
