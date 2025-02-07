53460688949272e898f12de0a9505ae70881ddf8
Sebastian <sebastian_bdavid@hotmail.com>
2025-02-07 15:17:38 -0500

    feat: setup the form - setup index.html - renamed HomeController to AccountOpenController

--- src/main/java/com/example/sebastian_301241956_assignment1/AccountOpenController.java
+++ src/main/java/com/example/sebastian_301241956_assignment1/AccountOpenController.java
@@ -3,7 +3,7 @@ package com.example.sebastian_301241956_assignment1;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
-public class HomeController {
+public class AccountOpenController {
     @GetMapping("/")
     public String home() {
         return "index";
